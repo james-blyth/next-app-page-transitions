d4e3bb9344331a2674ff05c7f666fd7dcd7c2d30
james-blyth <109626658+james-blyth@users.noreply.github.com>
2026-02-23 10:46:46 +0200

    Update next.config.js for static

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'export',
 	compiler: {
 		removeConsole: process.env.NODE_ENV === 'production',
 	},
 	images: {
 		formats: ['image/avif', 'image/webp'],
+		unoptimized: true,
 	},
 }
 
